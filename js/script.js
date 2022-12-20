e46cd6dbe927aa29250708f4b8697f44897567d6
arnab-4 <107208391+arnab-4@users.noreply.github.com>
2022-12-21 03:07:06 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,3 +37,20 @@ window.onscroll = () =>{
    document.querySelector('.home .cloud-1').style.right = `${-value}%`;
    document.querySelector('.home .cloud-2').style.left = `${-value}%`;
 };
+window.scroll({
+  top: 2500, 
+  left: 0, 
+  behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position 
+window.scrollBy({ 
+  top: 100, // could be negative value
+  left: 0, 
+  behavior: 'smooth' 
+});
+
+// Scroll to a certain element
+document.querySelector('.hello').scrollIntoView({ 
+  behavior: 'smooth' 
+});
